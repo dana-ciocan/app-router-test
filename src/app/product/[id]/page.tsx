4bd0d7697384d51cc24e7e0781d2f9cf3d7903ee
Dana Ciocan <danaciocan@economist.com>
2024-02-26 09:09:20 +0000

    Create product page and layout

---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,9 @@
+import styles from "../../page.module.css";
+
+export default function Product({ params }: { params: { id: string } }) {
+  return (
+    <main className={styles.main}>
+      This is the product page for ID: {params.id}
+    </main>
+  );
+}
