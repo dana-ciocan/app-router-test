36aaea2bdcf0172fdc50d911b02a44bcc6f50c6d
Dana Ciocan <danaciocan@economist.com>
2024-02-26 09:09:29 +0000

    Add link from homepage to product page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
         There isn&apos;t much here as it&apos;s just used to demo how routing
         works in the Next.js app router
       </p>
+      <Link href="/product/test-product">Go to a product page</Link>
     </main>
   );
 }
